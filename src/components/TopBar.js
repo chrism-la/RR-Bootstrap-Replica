384ca29ea31b8d499b463f02d58ec8c24c505515
Christopher <117781333+chrism-la@users.noreply.github.com>
2024-01-17 05:17:57 -0800

    utilizing vanilla css to create space between login and Brand image elements

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,22 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Navbar, NavbarCollapse, Nav } from 'react-bootstrap';
+import '../App.css';
 
 function TopBar() {
     return (
         <div>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand style={{ margin: '5px 0 0 20px' }} href="#home">
-                    <img src="instacart.png" width="300px" />
-                </Navbar.Brand>
+            <Navbar className="parent-div" bg="light" expand="lg">
+                <div>
+                    <Navbar.Brand style={{ margin: '5px 0 0 20px' }} href="#home">
+                        <img src="instacart.png" width="300px" />
+                    </Navbar.Brand>
+                </div>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
-                <NavbarCollapse id="basic-navbar-nav">
-                    <Nav className="mr-auto"></Nav>
-                    <Nav>
-                        <Nav.Link href="#LogIn">Log In</Nav.Link>
-                        <Button variant="success">Sign Up</Button>
-                        {''}
-                    </Nav>
-                </NavbarCollapse>
+                <div className="second-nav-child">
+                    <NavbarCollapse id="basic-navbar-nav">
+                        <Nav className="mr-auto"></Nav>
+                        <Nav>
+                            <Nav.Link href="#LogIn">Log In</Nav.Link>
+                            <Button variant="success">Sign Up</Button>
+                            {''}
+                        </Nav>
+                    </NavbarCollapse>
+                </div>
             </Navbar>
         </div>
     );

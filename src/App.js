a5f949e56dc45191944496da642167e052b95c22
Christopher <117781333+chrism-la@users.noreply.github.com>
2024-01-17 04:49:50 -0800

    imported necessary components and created NavBar elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import TopBar from './components/TopBar';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+    return (
+        <div>
+            <TopBar />
+        </div>
+    );
 }
 
 export default App;
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,2 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Navbar, Container } from 'bootstrap';
+import { Button, Navbar, NavbarCollapse, Nav } from 'react-bootstrap';
+
+function TopBar() {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand></Navbar.Brand>
+                <Navbar.Toggle></Navbar.Toggle>
+                <NavbarCollapse>
+                    <Nav></Nav>
+                    <Nav>
+                        <Nav.Link>Log In</Nav.Link>
+                        <Button>Sign Up</Button>
+                    </Nav>
+                </NavbarCollapse>
+            </Navbar>
+            ;
+        </div>
+    );
+}
+
+export default TopBar;

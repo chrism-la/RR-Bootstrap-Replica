2d738f4d9af092c87260f4b306dfa2ef0eca93df
Chris <117781333+chrism-la@users.noreply.github.com>
2024-01-17 17:33:59 -0800

    rendered StoreListing data

--- src/components/StoreListing.js
+++ src/components/StoreListing.js
@@ -1,5 +1,41 @@
+import { Container, Row, Col, Image } from 'react-bootstrap';
+
 function StoreListing() {
-    return <h2>StoreListing</h2>;
+    return (
+        <Container style={{ marginTop: '40px', textAlign: 'center' }}>
+            <Row style={{ borderBottom: '1px solid #E8E9EB', display: 'flex', justifyContent: 'space-evenly' }}>
+                <Row>
+                    <Col xs=".5">
+                        <Image src="" roundedCircle />
+                    </Col>
+                    <Col>
+                        <h5 style={{ fontWeight: 'bold' }}>ALDI</h5>
+                        <p>Delivery * Pickup</p>
+                    </Col>
+                </Row>
+                <Col sm="1"></Col>
+                <Row>
+                    <Col xs=".5">
+                        <Image src="sprouts.webp" roundedCircle />
+                    </Col>
+                    <Col>
+                        <h5 style={{ fontWeight: 'bold' }}>Sprouts Farmers Market</h5>
+                        <p>Delivery * Pickup</p>
+                    </Col>
+                </Row>
+                <Col sm="1"></Col>
+                <Row>
+                    <Col xs=".5">
+                        <Image src="" roundedCircle />
+                    </Col>
+                    <Col md="6">
+                        <h5 style={{ fontWeight: 'bold' }}>Kroger</h5>
+                        <p>Delivery</p>
+                    </Col>
+                </Row>
+            </Row>
+        </Container>
+    );
 }
 
 export default StoreListing;
